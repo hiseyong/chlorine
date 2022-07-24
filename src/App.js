c849a9b0a51bee705472a92f30c2c03a6fb0f6d8
hiseyong <hiseyong1008@gmail.com>
2022-07-24 21:23:49 +0900

    add interval

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import axios from 'axios';
 import './app.css'
 
 function App() {
   const client = axios.create();
+  const [activated, setActivated] = useState('')
   const [data, setData] = useState(50);
   const onClick = () => {
     client.get(`https://blynk.cloud/external/api/update?token=PCPldifAmHkzG2fmrG4WR9UzhZzPb5mi&v0=${data}`)
@@ -17,11 +18,39 @@ function App() {
     })
   };
 
+
+  useEffect(() => {
+    setInterval(() => {
+      client.get('https://blynk.cloud/external/api/isHardwareConnected?token=PCPldifAmHkzG2fmrG4WR9UzhZzPb5mi')
+    .then(res=> {
+      if (res.data === true) {
+        setActivated('online')
+      } else {
+        setActivated('offline')
+      }
+    })
+    }, 30000)
+    
+  }, [])
+
+  useEffect(() => {
+      client.get('https://blynk.cloud/external/api/isHardwareConnected?token=PCPldifAmHkzG2fmrG4WR9UzhZzPb5mi')
+    .then(res=> {
+      if (res.data === true) {
+        setActivated('online')
+      } else {
+        setActivated('offline')
+      }
+    })
+    
+  }, [])
+
   const onChange = (e) => {
     setData(e.target.value);
   }
   return (
     <div className="App">
+      <h3>machine is {activated}</h3>
       <center>
       <h1>{data}</h1>
       <input type="range" min="1" max="100" value={data} onChange={onChange}/>
